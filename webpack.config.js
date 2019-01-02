59597f1b5bdc1f225b23ea7b0d3570fd257d2d1d
Jesse R Mykolyn <jesse.mykolyn@groupbyinc.com>
2019-01-02 10:46:16 -0500

    Setup webpack

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.ts',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js',
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".json"]
+  },
+  module: {
+    rules: [
+      {
+        test: /.tsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'ts-loader',
+        },
+      },
+    ],
+  },
+};
